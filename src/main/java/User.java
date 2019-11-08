cfb8e78b42333212d18d88fef3be5febdcaf9d28
haining69 <2279798428@qq.com>
2019-11-08 11:15:52 +0800

    版本二

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,7 +1,9 @@
 import java.io.Serializable;
+import java.util.Date;
 
 public class User implements Serializable {
     private String username;
     private String password;
     private Integer age;
+    private Date birthday;
 }
